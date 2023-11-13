7a082152c5f5036991ba9724a6ed6351939369e1
Hoàng VănZ <ace30102002@gmail.com>
2023-11-13 21:09:31 +0700

    by hoang

---
+++ Areas/Student/Controllers/HomeStudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web2.Areas.Student.Controllers
+{
+    public class HomeStudentController : Controller
+    {
+        // GET: Student/HomeStudent
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
