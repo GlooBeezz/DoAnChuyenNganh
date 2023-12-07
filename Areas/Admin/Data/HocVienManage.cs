92af722e8d5a95f95ecc89c494bdc99949375254
Le Thanh Dat <lethanhdat1732002@gmail.com>
2023-12-08 03:23:26 +0700

    Dat-8/12-update

---
+++ Areas/Admin/Data/HocVienManage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using web2.Models;
+
+namespace web2.Areas.Admin.Data
+{
+    public class HocVienManage : IEnumerable<HocVien>
+    {
+        public List<HocVien> dsHV { get; set; }
+
+        public HocVienManage()
+        {
+            dsHV = new List<HocVien>();
+        }
+
+        // Thêm học viên vào danh sách
+        public void ThemHocVien(HocVien hocVien)
+        {
+            dsHV.Add(hocVien);
+        }
+
+        // Lấy danh sách học viên
+        public List<HocVien> LayDanhSachHocVien()
+        {
+            return dsHV;
+        }
+        public IEnumerator<HocVien> GetEnumerator()
+        {
+            return dsHV.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+    }
+}
---
+++ Areas/Student/Controllers/HomeStudentController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using web2.Areas.Student.Data;
+using web2.Models;
+namespace web2.Areas.Student.Controllers
+{
+    public class HomeStudentController : Controller
+    {
+        // GET: Student/HomeStudent
+        public ActionResult AboutStudent()
+        {
+            return View();
+        }
+        public ActionResult ContactStudent()
+        {
+            return View();
+        }
+       
+        public ActionResult indexHomeStudent()
+        {
+            return View();
+        }
+        public ActionResult LoginStudent()
+        {
+            return View();
+        }
+       
+        public ActionResult settingAccount()
+        {
+            return View();
+        }
+        public ActionResult ScheduleStudent()
+        {
+            return View();
+        }
+        public ActionResult CourseStudent( string  courseId)
+        {
+            KhoaHoc khoahoc = GetCourseInformation(courseId);
+
+            if (khoahoc == null)
+            {
+   
+                return HttpNotFound();
+            }
+            return View("CourseStudent", khoahoc); 
+        }
+        private KhoaHoc GetCourseInformation(string courseId)
+        {
+            KhoaHoc khoahoc = null; 
+            string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["quanLyTrungTamDayDanEntities3"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                string query = "SELECT Ma_Khoa_Hoc, Ten_Khoa_Hoc, Mo_Ta, Hoc_Phi, Nguon_anh FROM KhoaHoc WHERE Ma_Khoa_Hoc = @CourseId";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CourseId", courseId);
+                    using (SqlDataReader rd = cmd.ExecuteReader())
+                    {
+                        if (rd.Read())
+                        {
+                            khoahoc = new KhoaHoc
+                            {
+                                Ma_khoa_hoc = rd["Ma_Khoa_Hoc"].ToString(),
+                                Ten_khoa_hoc = rd["Ten_Khoa_Hoc"].ToString(),
+                                Mo_ta = rd["Mo_Ta"].ToString(),
+                                Hoc_phi = Convert.ToDecimal(rd["Hoc_Phi"]),
+                                Nguon_anh = rd["Nguon_Anh"].ToString()
+                            };
+                        }
+                    }
+                }
+            }
+
+            return khoahoc;
+        }
+        public ActionResult StudyStudent()
+        {
+            List<KhoaHoc> dsKH = new List<KhoaHoc>();
+
+            string connStr = System.Configuration.ConfigurationManager.ConnectionStrings["quanLyTrungTamDayDanEntities3"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT Ma_Khoa_Hoc, Ten_Khoa_Hoc, Mo_Ta, Hoc_Phi,Nguon_anh FROM KhoaHoc", conn))
+                {
+                    using (SqlDataReader rd = cmd.ExecuteReader())
+                    {
+                        while (rd.Read())
+                        {
+
+                            KhoaHoc kh = new KhoaHoc
+                            {
+                                Ma_khoa_hoc = rd["Ma_Khoa_Hoc"].ToString(),
+                                Ten_khoa_hoc = rd["Ten_Khoa_Hoc"].ToString(),
+                                Mo_ta = rd["Mo_Ta"].ToString(),
+                                Hoc_phi = Convert.ToDecimal(rd["Hoc_Phi"]),
+                                Nguon_anh = rd["Nguon_Anh"].ToString() 
+                            };
+                            dsKH.Add(kh);
+                        }
+                    }
+                }
+            }
+
+            return View(dsKH);
+        }
+    }
+}
