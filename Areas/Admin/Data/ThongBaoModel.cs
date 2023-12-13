218eaabe51a16d46cd4a3200a0935b198f28d41a
Hoàng VănZ <ace30102002@gmail.com>
2023-12-14 00:51:39 +0700

    create function send_notification

---
+++ Areas/Admin/Data/ThongBaoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using web2.Models;
+
+namespace web2.Areas.Admin.Data
+{
+    public class ThongBaoModel
+    {
+        public string NguoiNhan { get; set; }
+        public string Tieude { get; set; }
+        public string Noidung { get; set; }
+        public string maKhoaInput { get; set; }
+        public string maLopInput { get; set; }
+        public bool NguoiNhan_GiaoVien { get; set; }
+        public bool NguoiNhan_HocVien { get; set; }
+        public List<KhoaHoc> KhoaHocs { get; set; }
+        public List<LopHoc> LopHocs { get; set; }
+    }
+}
